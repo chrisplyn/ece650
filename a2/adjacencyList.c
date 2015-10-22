c1c7f1d58d0c538aa7d41f5c90df787e8d231829
chrisplyn <ascetic652@gmail.com>
2015-10-23 03:52:01 +0800

    ece650 assingment 2

---
+++ a2/adjacencyList.c
@@ -0,0 +1,48 @@
+/*
+ * adjacencyList.c
+ *
+ *  Created on: 2015Äê10ÔÂ21ÈÕ
+ *      Author: Administrator
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "adjacencyList.h"
+
+void initAdjList(adjList *adj, int numNodes, int elementSize) {
+	int i;
+	adj->numNodes = numNodes;
+	adj->l = (list *)malloc(numNodes*sizeof(list));
+
+	for (i = 0; i < numNodes; ++i) {
+		createList(adj->l+i, elementSize, NULL);
+	}
+}
+
+void freeAdjList(adjList *adj) {
+   int i;
+
+   for(i=0; i<adj->numNodes; ++i) {
+      destroy(adj->l+i);
+   }
+   free(adj->l);
+}
+
+void printAdjList(adjList *adj) {
+   int i;
+   listNode* current;
+
+   for (i=0; i<adj->numNodes; ++i) {
+      if(isEmptyList(adj->l+i)) {
+         continue;
+      }
+
+      current = ((adj->l)[i]).head;
+      printf("neighbor of node:%d is ", i);
+      while(current != NULL) {
+         printf("%d ", *(int *) current->data);
+         current = current->next;
+      }
+      printf("\n");
+   }
+}
---
+++ a2/main.c
@@ -0,0 +1,126 @@
+/*
+ * main.c
+ *
+ *  Created on: 2015Äê10ÔÂ20ÈÕ
+ *      Author: Administrator
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+#include "queue.h"
+#include "adjacencyList.h"
+
+
+void bfs(adjList* adj, int numNodes, int origin, int* prev) {
+	boolean visited[numNodes];
+	int i;
+	for (i = 0; i < numNodes; ++i) {
+		visited[i] = FALSE;
+		prev[i] = -1;
+	}
+
+	visited[origin] = TRUE;
+	queue q;
+	createQueue(&q, sizeof(int), NULL);
+	enqueue(&q, &origin);
+
+	while (!emptyQueue(&q)) {
+		void *data = dequeue(&q);
+		int u = *(int *) data;
+		listNode* current = (adj->l[u]).head;
+
+		while (current != NULL) {
+			int v = *(int *)current->data;
+
+			if (visited[v] == FALSE) {
+				visited[v] = TRUE;
+				enqueue(&q, &v);
+				prev[v] = u;
+			}
+
+			current = current->next;
+		}
+		free(data);
+	}
+	freeQueue(&q);
+}
+
+int main(int argc, char *argv[]) {
+   char *line = NULL, *p;
+   size_t size;
+   int numNodes, e1, e2, start, dest,*prev;
+   adjList adj;
+   int newline;
+
+   while ( (newline = getline(&line, &size, stdin)) != EOF) {
+      switch(*line) {
+         case 'V':
+            sscanf(line+2, "%d", &numNodes);
+            initAdjList(&adj, numNodes, sizeof(int));
+            break;
+         case 'E':
+            p = line+2;
+
+            while(*p != '}') {
+               sscanf(p+1,"<%d,%d>",&e1,&e2);
+               append(adj.l+e1,&e2);
+               append(adj.l+e2,&e1);
+               while(*p++ != '>');	// move pointer to position right after '>'
+            }
+
+            break;
+            case 's':
+               sscanf(line+2,"%d %d",&start,&dest);
+               list result;
+               createList(&result, sizeof(int), NULL);
+
+               if(start >= numNodes || start < 0) {
+                  printf("Error: Source vertex %d does not exist!\n", start);
+                  goto CLEANUP;
+               }
+
+               if(dest >= numNodes || dest < 0) {
+                  printf("Error: Destination vertex %d does not exist!\n", dest);
+                  goto CLEANUP;
+               }
+
+               prev = (int *)malloc(numNodes*sizeof(int));
+               bfs(&adj, numNodes, start, prev);
+               int cur = dest;
+
+               while(prev[cur] != -1) {
+                  prepend(&result,&cur);
+                  cur = prev[cur];
+               }
+
+               if(cur == start) {
+                  prepend(&result,&cur);
+               }
+               else {
+                  printf("Error: Path between vertex %d and vertex %d does not exist!\n", start, dest);
+                  goto CLEANUP;
+               }
+
+               listNode *node = result.head;
+
+               while(node != result.tail) {
+                  printf("%d-", *(int *) node->data);
+                  node = node->next;
+               }
+               printf("%d\n", *(int *) result.tail->data);
+
+               CLEANUP:
+                  if(result.length != 0) {
+                     destroy(&result);
+                  }
+
+                  freeAdjList(&adj);
+                  free(prev);
+                  break;
+      }
+   }
+
+   return 0;
+}
